9bcc5adbe960a01bca974784d50c24b882f1fd12
Moisés Santana <moisaant@gmail.com>
2023-09-12 01:00:14 -0300

    feat(highlight): create and styles

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,10 +1,15 @@
 import { Header } from '@components/header';
 import { Container } from './styles';
+import { Highlight } from '@components/highlight';
+
+const TITLE = "Turmas"
+const SUBTITLE = "jogue com sua turma"
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight title={ TITLE } subtitle={ SUBTITLE } />
     </Container>
   );
 }
