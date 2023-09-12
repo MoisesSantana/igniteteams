e8b99a1b6565dcc78d4d1d623edde3b0a1ddb147
Moisés Santana <moisaant@gmail.com>
2023-09-12 01:50:43 -0300

    feat(empty-list): create and style

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -2,17 +2,22 @@ import { Header } from '@components/header';
 import { Container } from './styles';
 import { Highlight } from '@components/highlight';
 import { GroupCard } from '@components/group-card';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/list-empty';
 
 const highlightProps = {
   title: "Turmas",
   subtitle: "jogue com sua turma"
 }
 
-const groupCardProps = {
-  title: "Turma 1"
+const listEmptyProps = {
+  message: "Que tal cadastrar a primeira turma?"
 }
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <Container>
       <Header />
@@ -20,7 +25,15 @@ export function Groups() {
         title={ highlightProps.title }
         subtitle={ highlightProps.subtitle }
       />
-      <GroupCard title={ groupCardProps.title } />
+      <FlatList
+        data={ groups }
+        keyExtractor={ item => item }
+        renderItem={ ({ item }) => (
+          <GroupCard title={ item } />
+        ) }
+        ListEmptyComponent={ () => <ListEmpty message={listEmptyProps.message} />}
+        contentContainerStyle={ groups.length === 0 && { flex: 1 } }
+      />
     </Container>
   );
 }
